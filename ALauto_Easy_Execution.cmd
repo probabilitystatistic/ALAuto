b30a7deb1dfffbd898bed26f342e544fa09d95c3
probabilitystatistic <probabilitystatistic@gmail.com>
2020-07-09 00:43:52 +0200

    Add commonly used config files and windows batch for easy execution.

---
+++ ALauto_Easy_Execution.cmd
@@ -0,0 +1,60 @@
+@ECHO OFF
+
+::Setting default path
+SET ALPATH=C:\Marsspi\Repository\ALAuto
+
+::Define the default map and mode
+SET PreviousMap=2-3
+SET PreviousMode=
+
+
+::Starting point for the loop
+:looping_the_execution
+
+
+:: choosing map
+SET /P Map= Which map?(2-3;5-1;6-1;7-2)
+
+IF "%Map%"=="2" (SET MAP=2-3)
+IF "%Map%"=="5" (SET MAP=5-1)
+IF "%Map%"=="6" (SET MAP=6-1)
+IF "%Map%"=="7" (SET MAP=7-2)
+IF "%Map%"==""  (SET MAP=%PreviousMap%)
+IF "%Map%"=="e" (GOTO :edit_by_hand)
+
+SET PreviousMap=%Map%
+
+SET FlagConfig=--c config_%Map%.ini
+
+
+:: choosing mode
+SET /P Mode= Using normal or debug mode?(n/d)
+
+IF "%Mode%"=="d" (SET FlagMode=--debug) ELSE (SET FlagMode= )
+
+SET PreviousMode=%FlagMode%
+
+ECHO "Program: python '%ALPATH%\ALAuto.py'"
+ECHO "Flags: %FlagConfig% %FlagMode%"
+
+powershell -Command "& {Set-Location -Path '%ALPATH%' -PassThru; python '%ALPATH%\ALAuto.py' %FlagConfig% %FlagMode%; }"
+
+GOTO :looping_the_execution
+
+
+:edit_by_hand
+notepad '%ALPATH%\config.ini'
+
+:: choosing mode
+SET /P Mode= Using normal or debug mode?(n/d)
+
+IF "%Mode%"=="d" (SET FlagMode=--debug) ELSE (SET FlagMode= )
+
+SET PreviousMode=%FlagMode%
+
+powershell -Command "& {Set-Location -Path '%ALPATH%' -PassThru; python '%ALPATH%\ALAuto.py' %FlagMode%; }"
+
+GOTO :looping_the_execution
+
+
+
