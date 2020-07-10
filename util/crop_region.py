bcbdac9bb175fadbaf9d904a992c763e43de8216
probabilitystatistic <probabilitystatistic@gmail.com>
2020-07-10 07:12:43 +0200

    Add the regions for find_with_cropped in the retirement module.

--- util/crop_region.py
+++ util/crop_region.py
@@ -6,8 +6,18 @@
 
 """
 #	def __init__(self):
-# form: x1,y1, x2, y2
+# form: x1, y1, x2, y2
 data = {
-    'menu/button_battle': [1500, 400, 1750, 700],
-    'commission/alert_completed': [965, 940, 955, 140]
+    "menu/alert_info": [0, 0, 960, 540], # Just a rough estimation!
+	"menu/build": [100, 0, 350, 100],
+    "menu/button_battle": [1500, 420, 1700, 600],
+    "menu/button_sort": [500, 700, 800, 850], # This is perhaps the sort button when port is full before starting a combat.
+    "menu/item_found": [700, 100, 1200, 550],
+    "commission/alert_completed": [1660, 400, 1760, 500],
+    "retirement/alert_bonus": [200, 900, 450, 1050],
+    "retirement/bonus": [460, 960, 620, 1060],
+    "retirement/button_disassemble": [1050, 800, 1380, 940], # The "confirm" when showing money earned from disassembling equipment.
+    "retirement/empty": [50, 450, 650, 650],
+    "retirement/no_batch": [300, 400, 1700, 550],
+    "retirement/selected_none": [650, 950, 800, 1080]
 }
