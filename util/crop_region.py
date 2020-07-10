d8d976b4419bc3172dcaeb31208abdc358015993
probabilitystatistic <probabilitystatistic@gmail.com>
2020-07-10 04:38:49 +0200

    Add a file containing information for crop region for cropped search.

---
+++ util/crop_region.py
@@ -0,0 +1,13 @@
+#from util.utils import Region
+
+
+#class CropRegionModule(object):
+""" All regions for cropped search(find_with_cropped) are store here.
+
+"""
+#	def __init__(self):
+# form: x1,y1, x2, y2
+data = {
+    'menu/button_battle': [1500, 400, 1750, 700],
+    'commission/alert_completed': [965, 940, 955, 140]
+}
