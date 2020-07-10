585c6f7d4b0f3b5f3da03bd9de1816e013b14421
probabilitystatistic <probabilitystatistic@gmail.com>
2020-07-10 19:01:18 +0200

    Adding related crop region for battle_handler in combat.py.

--- util/crop_region.py
+++ util/crop_region.py
@@ -8,12 +8,28 @@
 #	def __init__(self):
 # form: x1, y1, x2, y2
 data = {
+    "combat/alert_fleet_cannot_be_formed": [],
+    "combat/alert_lock": [],
+    "combat/alert_morale_low": [],
+    "combat/alert_unable_battle": [],
+    "combat/button_confirm": [],
+    "combat/button_retreat": [], 
+    "combat/combat_pause": [],
+    "combat/commander": [],
+    "combat/defeat_close_button": [],
+    "menu/button_confirm": [],
+    "menu/drop_common": [],
+    "menu/drop_elite": [],
+    "menu/drop_rare": [],
+    "menu/drop_ssr": [],
+    "combat/menu_touch2continue": [],
+    "combat/menu_loading": [],
+    "commission/alert_completed": [1660, 400, 1760, 500],
     "menu/alert_info": [0, 0, 960, 540], # Just a rough estimation!
 	"menu/build": [100, 0, 350, 100],
     "menu/button_battle": [1500, 420, 1700, 600],
     "menu/button_sort": [500, 700, 800, 850], # This is perhaps the sort button when port is full before starting a combat.
     "menu/item_found": [700, 100, 1200, 550],
-    "commission/alert_completed": [1660, 400, 1760, 500],
     "retirement/alert_bonus": [200, 900, 450, 1050],
     "retirement/bonus": [460, 960, 620, 1060],
     "retirement/button_disassemble": [1050, 800, 1380, 940], # The "confirm" when showing money earned from disassembling equipment.
